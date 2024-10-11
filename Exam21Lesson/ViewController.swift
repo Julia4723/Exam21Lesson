5c4762f1296e0fa2b3b1f057d36965b36c768eaa
My <uk472363@yanex.ru>
2024-10-11 09:45:09 +0300

    Initial Commit

---
+++ Exam21Lesson/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Exam21Lesson
+//
+//  Created by user on 11.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
