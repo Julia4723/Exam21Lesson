dd543e14fd1d48d279e67d929d2cf09e22981ab6
My <uk472363@yanex.ru>
2024-10-24 08:11:32 +0300

    refactor lesson27

--- Exam21Lesson/Controllers/ViewController.swift
+++ Exam21Lesson/Controllers/ViewController.swift
@@ -30,7 +30,7 @@ final class ViewController: UIViewController, ICustomButtonDelegate {
     private let text = UILabel()
     private let stackView = UIStackView()
     private let stackTop = UIStackView()
-    
+   
     
     //MARK: - Init
     
@@ -84,12 +84,12 @@ final class ViewController: UIViewController, ICustomButtonDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         imageView.isUserInteractionEnabled = true
         
-        let touch: UITouch = touches.first!
-        
-        if (touch.view == imageView) {
-            let current = dataManager?.getCurrentExample()
-            text.text = current?.imageName
-            print(current?.imageName)
+        for touch in touches {
+            if (touch.view != nil) {
+                let current = dataManager?.getCurrentExample()
+                text.text = current?.imageName
+                print(current?.imageName)
+            }
         }
     }
     
